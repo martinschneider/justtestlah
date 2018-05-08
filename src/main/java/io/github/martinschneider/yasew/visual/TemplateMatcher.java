2c81f50d4f09710ec0f6fa6fea5f08af3781d68c
Martin Schneider <mart.schneider@gmail.com>
2018-05-08 11:33:40 +0800

    Fixes and adaptions in TemplateMatcher Match is marked in the image and the result saved in the target folder

--- src/main/java/io/github/martinschneider/yasew/visual/TemplateMatcher.java
+++ src/main/java/io/github/martinschneider/yasew/visual/TemplateMatcher.java
@@ -1,10 +1,14 @@
 package io.github.martinschneider.yasew.visual;
 
 import io.github.martinschneider.yasew.configuration.YasewConfiguration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.opencv.core.Core;
 import org.opencv.core.Core.MinMaxLocResult;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
@@ -28,35 +32,52 @@ public class TemplateMatcher {
   private static final int MAX_IMAGE_WIDTH = 2048;
 
   private static final Logger LOG = LoggerFactory.getLogger(TemplateMatcher.class);
+  private static final String FILE_EXTENSION = "png";
+  private static final String DATE_PATTERN = "yyyy-MM-dd HH.mm.ss";
 
   private YasewConfiguration configuration;
 
   /**
    * Check whether the template appears anywhere within the target image.
-   * 
+   *
    * @param targetFile path to the target file
    * @param templateFile path to the template file
    * @param threshold matching threshold
    * @return true if a match has been found within the given threshold
    */
   public boolean match(String targetFile, String templateFile, double threshold) {
+    return match(
+        targetFile,
+        templateFile,
+        threshold,
+        LocalDateTime.now().format(DateTimeFormatter.ofPattern(DATE_PATTERN)));
+  }
+
+  /**
+   * Check whether the template appears anywhere within the target image.
+   *
+   * @param targetFile path to the target file
+   * @param templateFile path to the template file
+   * @param threshold matching threshold
+   * @param description of the check
+   * @return true if a match has been found within the given threshold
+   */
+  public boolean match(
+      String targetFile, String templateFile, double threshold, String description) {
     checkForOpenCv();
     Mat image = Imgcodecs.imread(targetFile);
-    Mat originalImage = image;
     Mat templ = Imgcodecs.imread(templateFile);
-
-    // / Create the result matrix
-    int resultCols = image.cols() - templ.cols() + 1;
-    int resultRows = image.rows() - templ.rows() + 1;
-    Mat result = new Mat(resultRows, resultCols, CvType.CV_32FC1);
     MinMaxLocResult bestMatch = new MinMaxLocResult();
-
+    Mat originalImage = image;
     /**
      * This could be improved by combining the two while loops (checking the original size first,
      * then slightly smaller and larger images etc. instead of first scaling all the way down and
      * then scaling up (or by using a framework that performs size-invariant template matching).
      */
     while (image.width() > MIN_IMAGE_WIDTH) {
+      int resultCols = image.cols() - templ.cols() + 1;
+      int resultRows = image.rows() - templ.rows() + 1;
+      Mat result = new Mat(resultRows, resultCols, CvType.CV_32FC1);
       Imgproc.matchTemplate(image, templ, result, Imgproc.TM_CCOEFF_NORMED);
       MinMaxLocResult match = Core.minMaxLoc(result);
       if (match.maxVal > bestMatch.maxVal) {
@@ -68,8 +89,13 @@ public class TemplateMatcher {
       // else
       image = scaleImage(image, 0.9);
     }
-    image = originalImage;
+    if (bestMatch.maxVal < threshold) {
+      image = originalImage;
+    }
     while (bestMatch.maxVal < threshold && image.width() < MAX_IMAGE_WIDTH) {
+      int resultCols = image.cols() - templ.cols() + 1;
+      int resultRows = image.rows() - templ.rows() + 1;
+      Mat result = new Mat(resultRows, resultCols, CvType.CV_32FC1);
       Imgproc.matchTemplate(image, templ, result, Imgproc.TM_CCOEFF_NORMED);
       MinMaxLocResult match = Core.minMaxLoc(result);
       if (match.maxVal > bestMatch.maxVal) {
@@ -87,6 +113,18 @@ public class TemplateMatcher {
           targetFile,
           templateFile,
           bestMatch.maxVal);
+
+      Imgproc.rectangle(
+          image,
+          new Point(bestMatch.maxLoc.x, bestMatch.maxLoc.y),
+          new Point(bestMatch.maxLoc.x + templ.cols(), bestMatch.maxLoc.y + templ.rows()),
+          new Scalar(255, 0, 0),
+          10);
+
+      String fileName =
+          System.getProperty("user.dir") + "/target/" + description + "." + FILE_EXTENSION;
+      LOG.info("Writing result of template matching to {}", fileName);
+      Imgcodecs.imwrite(fileName, image);
       return true;
     }
     // else
--- src/test/java/io/github/martinschneider/yasew/visual/TemplateMatcherTest.java
+++ src/test/java/io/github/martinschneider/yasew/visual/TemplateMatcherTest.java
@@ -14,9 +14,7 @@ public class TemplateMatcherTest {
 
   private static TemplateMatcher target = new TemplateMatcher();
 
-  /**
-   * Initialise mocks and configuration.
-   */
+  /** Initialise mocks and configuration. */
   @BeforeClass
   public static void init() {
     OpenCV.loadShared();
@@ -29,24 +27,56 @@ public class TemplateMatcherTest {
 
   @Test
   public void testMatchPerfect() {
-
-    assertThat(target.match(getPath("largerMatch.png"), getPath("icon.png"), 0.9)).isTrue();
-    assertThat(target.match(getPath("perfectMatch.png"), getPath("icon.png"), 1)).isTrue();
+    assertThat(
+            target.match(getPath("perfectMatch.png"), getPath("questionIcon.png"), 1, "sameSize"))
+        .isTrue();
   }
 
   @Test
   public void testMatchScaleDown() {
-    assertThat(target.match(getPath("smallerMatch.png"), getPath("icon.png"), 0.9)).isTrue();
+    assertThat(
+            target.match(
+                getPath("smallerMatch.png"), getPath("questionIcon.png"), 0.9, "scaleDown"))
+        .isTrue();
   }
 
   @Test
   public void testMatchScaleUp() {
-    assertThat(target.match(getPath("largerMatch.png"), getPath("icon.png"), 0.9)).isTrue();
+    assertThat(
+            target.match(getPath("largerMatch.png"), getPath("questionIcon.png"), 0.9, "scaleUp"))
+        .isTrue();
+  }
+
+  @Test
+  public void testBlurred() {
+    assertThat(
+            target.match(
+                getPath("perfectMatch.png"), getPath("questionIcon_blurred.png"), 0.9, "blurred"))
+        .isTrue();
+  }
+
+  @Test
+  public void testDistorted() {
+    assertThat(
+            target.match(
+                getPath("perfectMatch.png"),
+                getPath("questionIcon_distorted.png"),
+                0.75,
+                "distorted"))
+        .isTrue();
+  }
+
+  @Test
+  public void testRotated() {
+    assertThat(
+            target.match(
+                getPath("perfectMatch.png"), getPath("questionIcon_rotated.png"), 0.85, "rotated"))
+        .isTrue();
   }
 
   @Test
   public void testNoMatch() {
-    assertThat(target.match(getPath("noMatch.png"), getPath("icon.png"), 0.5)).isFalse();
+    assertThat(target.match(getPath("noMatch.png"), getPath("questionIcon.png"), 0.5)).isFalse();
   }
 
   private String getPath(String fileName) {
