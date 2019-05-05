4647ec496ae6bce9b051116622ec1c48aa4da652
Martin Schneider <mart.schneider@gmail.com>
2019-05-05 16:13:23 +0800

    Unit tests

--- justtestlah-core/src/test/java/io/github/martinschneider/justtestlah/junit/JustTestLahRunnerTest.java
+++ justtestlah-core/src/test/java/io/github/martinschneider/justtestlah/junit/JustTestLahRunnerTest.java
@@ -2,24 +2,59 @@ package io.github.martinschneider.justtestlah.junit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import io.github.martinschneider.justtestlah.configuration.PropertiesHolder;
 import java.io.IOException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
 import org.junit.runners.model.InitializationError;
 
+@RunWith(DataProviderRunner.class)
 public class JustTestLahRunnerTest {
 
   private JustTestLahRunner target;
 
   @Test
-  public void testBuildCucumberOptions() throws InitializationError, IOException {
+  @UseDataProvider("testData")
+  public void testBuildCucumberOptions(String propertiesFile, String expectedCucumberOptions)
+      throws InitializationError, IOException {
     System.setProperty(
         PropertiesHolder.JUST_TEST_LAH_LOCATION_KEY,
-        JustTestLahRunnerTest.class.getResource("justtestlah.properties").getFile());
+        JustTestLahRunnerTest.class.getResource(propertiesFile).getFile());
     target = new JustTestLahRunner(this.getClass());
     assertThat(target.buildCucumberOptions())
         .as("check cucumber options")
-        .isEqualTo(
-            "--tags @web --tags @regression --glue io.github.martinschneider.justtestlah.steps  --glue com.test --plugin pretty --plugin html:report --plugin json:target/report/cucumber/cucumber.json src/test/resources/features --strict");
+        .isEqualTo(expectedCucumberOptions);
+  }
+
+  @Rule public ExpectedException exceptionRule = ExpectedException.none();
+
+  @Test
+  public void testInvalidCharactersInCucumberOptions() throws InitializationError, IOException {
+    exceptionRule.expect(RuntimeException.class);
+    exceptionRule.expectMessage("Invalid character ' in tag expression: '");
+    System.setProperty(
+        PropertiesHolder.JUST_TEST_LAH_LOCATION_KEY,
+        JustTestLahRunnerTest.class.getResource("justtestlah_injection.properties").getFile());
+    target = new JustTestLahRunner(this.getClass());
+    assertThat(target.buildCucumberOptions()).as("check cucumber options");
+  }
+
+  @DataProvider
+  public static Object[][] testData() {
+    return new Object[][] {
+      {
+        "justtestlah_legacy.properties",
+        "--tags '@web and @regression' --glue io.github.martinschneider.justtestlah.steps  --glue com.test --plugin pretty --plugin html:report --plugin json:target/report/cucumber/cucumber.json src/test/resources/features --strict"
+      },
+      {
+        "justtestlah_tagexpression.properties",
+        "--tags '@web and (@regression or not @skip)' --glue io.github.martinschneider.justtestlah.steps  --glue com.test --plugin pretty --plugin html:report --plugin json:target/report/cucumber/cucumber.json src/test/resources/features --strict"
+      }
+    };
   }
 }
