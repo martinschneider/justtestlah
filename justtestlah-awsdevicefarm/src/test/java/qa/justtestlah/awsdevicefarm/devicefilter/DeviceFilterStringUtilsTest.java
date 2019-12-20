5c3feddf451f40712e9336f0d29bde0b794174a4
Martin Schneider <mart.schneider@gmail.com>
2019-12-20 11:53:52 +0800

    tests

--- justtestlah-awsdevicefarm/src/test/java/qa/justtestlah/awsdevicefarm/devicefilter/DeviceFilterStringUtilsTest.java
+++ justtestlah-awsdevicefarm/src/test/java/qa/justtestlah/awsdevicefarm/devicefilter/DeviceFilterStringUtilsTest.java
@@ -1,7 +1,6 @@
 package qa.justtestlah.awsdevicefarm.devicefilter;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import com.amazonaws.services.devicefarm.model.DeviceFilter;
 import com.amazonaws.services.devicefarm.model.RuleOperator;
 import java.util.ArrayList;
@@ -12,30 +11,84 @@ import org.junit.Test;
 public class DeviceFilterStringUtilsTest {
 
   @Test
-  public void testSingleFilter() {
+  public void testEqualsFilter() {
     List<DeviceFilter> deviceFilterList = new ArrayList<DeviceFilter>();
-    deviceFilterList.add(
-        new DeviceFilter()
-            .withAttribute("attribute1")
-            .withOperator(RuleOperator.EQUALS)
-            .withValues("value1"));
+    deviceFilterList.add(new DeviceFilter().withAttribute("attribute1")
+        .withOperator(RuleOperator.EQUALS).withValues("value1"));
     assertThat(DeviceFilterStringUtils.prettyPrintDeviceFilterList(deviceFilterList))
         .isEqualTo("[attribute1=value1]");
   }
 
   @Test
+  public void testContainsFilter() {
+    List<DeviceFilter> deviceFilterList = new ArrayList<DeviceFilter>();
+    deviceFilterList.add(new DeviceFilter().withAttribute("attribute1")
+        .withOperator(RuleOperator.CONTAINS).withValues("value1"));
+    assertThat(DeviceFilterStringUtils.prettyPrintDeviceFilterList(deviceFilterList))
+        .isEqualTo("[attribute1∋value1]");
+  }
+
+  @Test
+  public void testInFilter() {
+    List<DeviceFilter> deviceFilterList = new ArrayList<DeviceFilter>();
+    deviceFilterList.add(new DeviceFilter().withAttribute("attribute1")
+        .withOperator(RuleOperator.IN).withValues("value1"));
+    assertThat(DeviceFilterStringUtils.prettyPrintDeviceFilterList(deviceFilterList))
+        .isEqualTo("[attribute1∈value1]");
+  }
+
+  @Test
+  public void testNotInFilter() {
+    List<DeviceFilter> deviceFilterList = new ArrayList<DeviceFilter>();
+    deviceFilterList.add(new DeviceFilter().withAttribute("attribute1")
+        .withOperator(RuleOperator.NOT_IN).withValues("value1"));
+    assertThat(DeviceFilterStringUtils.prettyPrintDeviceFilterList(deviceFilterList))
+        .isEqualTo("[attribute1∉value1]");
+  }
+
+  @Test
+  public void testLessThanFilter() {
+    List<DeviceFilter> deviceFilterList = new ArrayList<DeviceFilter>();
+    deviceFilterList.add(new DeviceFilter().withAttribute("attribute1")
+        .withOperator(RuleOperator.LESS_THAN).withValues("value1"));
+    assertThat(DeviceFilterStringUtils.prettyPrintDeviceFilterList(deviceFilterList))
+        .isEqualTo("[attribute1<value1]");
+  }
+
+  @Test
+  public void testLessThanOrEqualsFilter() {
+    List<DeviceFilter> deviceFilterList = new ArrayList<DeviceFilter>();
+    deviceFilterList.add(new DeviceFilter().withAttribute("attribute1")
+        .withOperator(RuleOperator.LESS_THAN_OR_EQUALS).withValues("value1"));
+    assertThat(DeviceFilterStringUtils.prettyPrintDeviceFilterList(deviceFilterList))
+        .isEqualTo("[attribute1≤value1]");
+  }
+
+  @Test
+  public void testGreaterThanFilter() {
+    List<DeviceFilter> deviceFilterList = new ArrayList<DeviceFilter>();
+    deviceFilterList.add(new DeviceFilter().withAttribute("attribute1")
+        .withOperator(RuleOperator.GREATER_THAN).withValues("value1"));
+    assertThat(DeviceFilterStringUtils.prettyPrintDeviceFilterList(deviceFilterList))
+        .isEqualTo("[attribute1>value1]");
+  }
+
+  @Test
+  public void testGreaterThanOrEqualsFilter() {
+    List<DeviceFilter> deviceFilterList = new ArrayList<DeviceFilter>();
+    deviceFilterList.add(new DeviceFilter().withAttribute("attribute1")
+        .withOperator(RuleOperator.GREATER_THAN_OR_EQUALS).withValues("value1"));
+    assertThat(DeviceFilterStringUtils.prettyPrintDeviceFilterList(deviceFilterList))
+        .isEqualTo("[attribute1≥value1]");
+  }
+
+  @Test
   public void testMutlipleFilters() {
     List<DeviceFilter> deviceFilterList = new ArrayList<DeviceFilter>();
-    deviceFilterList.add(
-        new DeviceFilter()
-            .withAttribute("attribute1")
-            .withOperator(RuleOperator.EQUALS)
-            .withValues("value1"));
-    deviceFilterList.add(
-        new DeviceFilter()
-            .withAttribute("attribute2")
-            .withOperator(RuleOperator.LESS_THAN)
-            .withValues("value2"));
+    deviceFilterList.add(new DeviceFilter().withAttribute("attribute1")
+        .withOperator(RuleOperator.EQUALS).withValues("value1"));
+    deviceFilterList.add(new DeviceFilter().withAttribute("attribute2")
+        .withOperator(RuleOperator.LESS_THAN).withValues("value2"));
     assertThat(DeviceFilterStringUtils.prettyPrintDeviceFilterList(deviceFilterList))
         .isEqualTo("[attribute1=value1], [attribute2<value2]");
   }
