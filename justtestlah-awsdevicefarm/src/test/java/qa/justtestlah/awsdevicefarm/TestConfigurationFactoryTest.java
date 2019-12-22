07eba6f2b0cb30b26254cd8602209ffc7013fe2c
Martin Schneider <mart.schneider@gmail.com>
2019-12-22 11:48:11 +0800

    unit test

--- justtestlah-awsdevicefarm/src/test/java/qa/justtestlah/awsdevicefarm/TestConfigurationFactoryTest.java
+++ justtestlah-awsdevicefarm/src/test/java/qa/justtestlah/awsdevicefarm/TestConfigurationFactoryTest.java
@@ -1,43 +1,210 @@
 package qa.justtestlah.awsdevicefarm;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
 
+import com.amazonaws.services.devicefarm.model.BillingMethod;
 import com.amazonaws.services.devicefarm.model.ExecutionConfiguration;
-import org.junit.Before;
+import com.amazonaws.services.devicefarm.model.ScheduleRunConfiguration;
+import java.util.Properties;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.test.util.ReflectionTestUtils;
 import qa.justtestlah.configuration.PropertiesHolder;
 
 public class TestConfigurationFactoryTest {
 
   private TestConfigurationFactory target;
 
-  @Mock private PropertiesHolder properties;
+  private PropertiesHolder properties = new PropertiesHolder();
 
-  @Before
-  public void setUp() {
-    MockitoAnnotations.initMocks(this);
-    when(properties.getProperty(eq("aws.accountsCleanup"), anyString())).thenReturn("true");
-    when(properties.getProperty(eq("aws.appPackagesCleanUp"), anyString())).thenReturn("true");
-    when(properties.getProperty(eq("aws.jobTimeout"), anyString())).thenReturn("10");
-    when(properties.getProperty(eq("aws.skipAppResign"), anyString())).thenReturn("true");
+  @Test
+  public void testScheduleRunConfigurationTestTrue() {
+    Properties props = new Properties();
+    props.put("aws.runUnmetered", "true");
+    props.put("aws.deviceLatitude", "100.0");
+    props.put("aws.deviceLongitude", "50.0");
+    props.put("aws.bluetooth", "true");
+    props.put("aws.gps", "true");
+    props.put("aws.nfc", "true");
+    props.put("aws.wifi", "true");
+    props.put("aws.extraDataArn", "extraPackage");
+    ReflectionTestUtils.setField(properties, "props", props);
     target = new TestConfigurationFactory(properties);
+
+    ScheduleRunConfiguration scheduleRunConfiguration = target.getScheduleRunConfiguration();
+    BillingMethod billingMethod =
+        Boolean.parseBoolean(props.getProperty("aws.runUnmetered"))
+            ? BillingMethod.UNMETERED
+            : BillingMethod.METERED;
+    assertThat(scheduleRunConfiguration.getBillingMethod()).isEqualTo(billingMethod.toString());
+    assertThat(scheduleRunConfiguration.getLocation().getLatitude())
+        .isEqualTo(Double.parseDouble(props.getProperty("aws.deviceLatitude")));
+    assertThat(scheduleRunConfiguration.getLocation().getLongitude())
+        .isEqualTo(Double.parseDouble(props.getProperty("aws.deviceLongitude")));
+    assertThat(scheduleRunConfiguration.getRadios().getBluetooth())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.bluetooth")));
+    assertThat(scheduleRunConfiguration.getRadios().getGps())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.gps")));
+    assertThat(scheduleRunConfiguration.getRadios().getNfc())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.nfc")));
+    assertThat(scheduleRunConfiguration.getRadios().getWifi())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.wifi")));
+    assertThat(scheduleRunConfiguration.getExtraDataPackageArn())
+        .isEqualTo(props.getProperty("aws.extraDataArn"));
   }
 
   @Test
-  public void testExecutionConfigurationTest() {
+  public void testScheduleRunConfigurationTestFalse() {
+    Properties props = new Properties();
+    props.put("aws.runUnmetered", "false");
+    props.put("aws.bluetooth", "false");
+    props.put("aws.gps", "false");
+    props.put("aws.nfc", "false");
+    props.put("aws.wifi", "false");
+    ReflectionTestUtils.setField(properties, "props", props);
+    target = new TestConfigurationFactory(properties);
+
+    ScheduleRunConfiguration scheduleRunConfiguration = target.getScheduleRunConfiguration();
+    BillingMethod billingMethod =
+        Boolean.parseBoolean(props.getProperty("aws.runUnmetered"))
+            ? BillingMethod.UNMETERED
+            : BillingMethod.METERED;
+    assertThat(scheduleRunConfiguration.getBillingMethod()).isEqualTo(billingMethod.toString());
+    assertThat(scheduleRunConfiguration.getRadios().getBluetooth())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.bluetooth")));
+    assertThat(scheduleRunConfiguration.getRadios().getGps())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.gps")));
+    assertThat(scheduleRunConfiguration.getRadios().getNfc())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.nfc")));
+    assertThat(scheduleRunConfiguration.getRadios().getWifi())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.wifi")));
+  }
+
+  @Test
+  public void testScheduleRunConfigurationTestRandom() {
+    Properties props = new Properties();
+    props.put("aws.runUnmetered", Boolean.toString(RandomUtils.nextBoolean()));
+    props.put("aws.deviceLatitude", Double.toString(RandomUtils.nextDouble(0, 180)));
+    props.put("aws.deviceLongitude", Double.toString(RandomUtils.nextDouble(0, 180)));
+    props.put("aws.bluetooth", Boolean.toString(RandomUtils.nextBoolean()));
+    props.put("aws.gps", Boolean.toString(RandomUtils.nextBoolean()));
+    props.put("aws.nfc", Boolean.toString(RandomUtils.nextBoolean()));
+    props.put("aws.wifi", Boolean.toString(RandomUtils.nextBoolean()));
+    props.put("aws.extraDataArn", RandomStringUtils.randomAlphanumeric(20));
+    ReflectionTestUtils.setField(properties, "props", props);
+    target = new TestConfigurationFactory(properties);
+
+    ScheduleRunConfiguration scheduleRunConfiguration = target.getScheduleRunConfiguration();
+    BillingMethod billingMethod =
+        Boolean.parseBoolean(props.getProperty("aws.runUnmetered"))
+            ? BillingMethod.UNMETERED
+            : BillingMethod.METERED;
+    assertThat(scheduleRunConfiguration.getBillingMethod()).isEqualTo(billingMethod.toString());
+    assertThat(scheduleRunConfiguration.getLocation().getLatitude())
+        .isEqualTo(Double.parseDouble(props.getProperty("aws.deviceLatitude")));
+    assertThat(scheduleRunConfiguration.getLocation().getLongitude())
+        .isEqualTo(Double.parseDouble(props.getProperty("aws.deviceLongitude")));
+    assertThat(scheduleRunConfiguration.getRadios().getBluetooth())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.bluetooth")));
+    assertThat(scheduleRunConfiguration.getRadios().getGps())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.gps")));
+    assertThat(scheduleRunConfiguration.getRadios().getNfc())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.nfc")));
+    assertThat(scheduleRunConfiguration.getRadios().getWifi())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.wifi")));
+    assertThat(scheduleRunConfiguration.getExtraDataPackageArn())
+        .isEqualTo(props.getProperty("aws.extraDataArn"));
+  }
+
+  @Test
+  public void testScheduleRunConfigurationTestDefaults() {
+    Properties props = new Properties();
+    ReflectionTestUtils.setField(properties, "props", props);
+    target = new TestConfigurationFactory(properties);
+
+    ScheduleRunConfiguration scheduleRunConfiguration = target.getScheduleRunConfiguration();
+    assertThat(scheduleRunConfiguration.getBillingMethod())
+        .isEqualTo(BillingMethod.UNMETERED.toString());
+    assertThat(scheduleRunConfiguration.getLocation().getLatitude()).isEqualTo(1.3521);
+    assertThat(scheduleRunConfiguration.getLocation().getLongitude()).isEqualTo(103.8198);
+    assertThat(scheduleRunConfiguration.getRadios().getBluetooth()).isEqualTo(false);
+    assertThat(scheduleRunConfiguration.getRadios().getGps()).isEqualTo(true);
+    assertThat(scheduleRunConfiguration.getRadios().getNfc()).isEqualTo(true);
+    assertThat(scheduleRunConfiguration.getRadios().getWifi()).isEqualTo(true);
+    assertThat(scheduleRunConfiguration.getExtraDataPackageArn()).isEqualTo(null);
+  }
+
+  @Test
+  public void testExecutionConfigurationTestTrue() {
+    Properties props = new Properties();
+    props.put("aws.accountsCleanUp", "true");
+    props.put("aws.appPackagesCleanUp", "true");
+    props.put("aws.jobTimeout", "10");
+    props.put("aws.skipAppResign", "true");
+    ReflectionTestUtils.setField(properties, "props", props);
+    target = new TestConfigurationFactory(properties);
+
     ExecutionConfiguration executionConfiguration = target.getExecutionConfiguration();
     assertThat(executionConfiguration.getAccountsCleanup())
-        .isEqualTo(Boolean.parseBoolean(properties.getProperty("aws.accountsCleanup", "true")));
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.accountsCleanUp")));
     assertThat(executionConfiguration.getAppPackagesCleanup())
-        .isEqualTo(Boolean.parseBoolean(properties.getProperty("aws.appPackagesCleanUp", "true")));
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.appPackagesCleanUp")));
     assertThat(executionConfiguration.getJobTimeoutMinutes())
-        .isEqualTo(Integer.parseInt(properties.getProperty("aws.jobTimeout", "10")));
+        .isEqualTo(Integer.parseInt(props.getProperty("aws.jobTimeout")));
     assertThat(executionConfiguration.getSkipAppResign())
-        .isEqualTo(Boolean.parseBoolean(properties.getProperty("aws.skipAppResign", "true")));
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.skipAppResign")));
+  }
+
+  @Test
+  public void testExecutionConfigurationTestRandom() {
+    Properties props = new Properties();
+    props.put("aws.accountsCleanUp", Boolean.toString(RandomUtils.nextBoolean()));
+    props.put("aws.appPackagesCleanUp", Boolean.toString(RandomUtils.nextBoolean()));
+    props.put("aws.jobTimeout", Integer.toString(RandomUtils.nextInt()));
+    props.put("aws.skipAppResign", Boolean.toString(RandomUtils.nextBoolean()));
+    ReflectionTestUtils.setField(properties, "props", props);
+    target = new TestConfigurationFactory(properties);
+
+    ExecutionConfiguration executionConfiguration = target.getExecutionConfiguration();
+    assertThat(executionConfiguration.getAccountsCleanup())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.accountsCleanUp")));
+    assertThat(executionConfiguration.getAppPackagesCleanup())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.appPackagesCleanUp")));
+    assertThat(executionConfiguration.getJobTimeoutMinutes())
+        .isEqualTo(Integer.parseInt(props.getProperty("aws.jobTimeout")));
+    assertThat(executionConfiguration.getSkipAppResign())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.skipAppResign")));
+  }
+
+  @Test
+  public void testExecutionConfigurationTestFalse() {
+    Properties props = new Properties();
+    props.put("aws.accountsCleanUp", "false");
+    props.put("aws.appPackagesCleanUp", "false");
+    props.put("aws.skipAppResign", "false");
+    ReflectionTestUtils.setField(properties, "props", props);
+    target = new TestConfigurationFactory(properties);
+
+    ExecutionConfiguration executionConfiguration = target.getExecutionConfiguration();
+    assertThat(executionConfiguration.getAccountsCleanup())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.accountsCleanUp")));
+    assertThat(executionConfiguration.getAppPackagesCleanup())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.appPackagesCleanUp")));
+    assertThat(executionConfiguration.getSkipAppResign())
+        .isEqualTo(Boolean.parseBoolean(props.getProperty("aws.skipAppResign")));
+  }
+
+  @Test
+  public void testExecutionConfigurationTestDefaults() {
+    Properties props = new Properties();
+    ReflectionTestUtils.setField(properties, "props", props);
+    target = new TestConfigurationFactory(properties);
+
+    ExecutionConfiguration executionConfiguration = target.getExecutionConfiguration();
+    assertThat(executionConfiguration.getAccountsCleanup()).isEqualTo(true);
+    assertThat(executionConfiguration.getAppPackagesCleanup()).isEqualTo(true);
+    assertThat(executionConfiguration.getJobTimeoutMinutes()).isEqualTo(10);
+    assertThat(executionConfiguration.getSkipAppResign()).isEqualTo(false);
   }
 }
