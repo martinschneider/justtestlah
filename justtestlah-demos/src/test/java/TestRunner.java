0515d9f60fc38ce63f1bea7f64996de0262d41c6
Martin Schneider <mart.schneider@gmail.com>
2020-02-12 14:58:20 +0800

    Release 1.8-RC2
    Small fixes and changes for AWS Devicefarm execution

---
+++ justtestlah-demos/src/test/java/TestRunner.java
@@ -0,0 +1,5 @@
+import qa.justtestlah.junit.JustTestLahTest;
+
+@SuppressWarnings("PMD.DefaultPackage")
+// this class uses the default package for convenient usage
+public class TestRunner extends JustTestLahTest {}
