42ba87f03dce00f6975463b42ae3942c9fb2aea2
Martin Schneider <mart.schneider@gmail.com>
2021-01-01 14:59:24 +0800

    Remove @PostConstruct annotations.

--- justtestlah-core/src/main/java/qa/justtestlah/base/Base.java
+++ justtestlah-core/src/main/java/qa/justtestlah/base/Base.java
@@ -1,10 +1,10 @@
 package qa.justtestlah.base;
 
 import java.lang.reflect.Field;
-import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
@@ -14,16 +14,19 @@ import org.springframework.context.ApplicationContextAware;
  * <p>We inject all page objects. This way it is not necessary to annotate them with {@link
  * org.springframework.beans.factory.annotation.Autowired}.
  */
-public class Base implements ApplicationContextAware {
+public class Base implements ApplicationContextAware, InitializingBean{
 
   private static final Logger LOG = LoggerFactory.getLogger(Base.class);
   private ApplicationContext applicationContext;
 
+  @Override
+  public void afterPropertiesSet() {
+    initPages();
+  }
   /**
    * inject the page objects (without using @org.springframework.beans.factory.annotation.Autowired
    * annotations)
    */
-  @PostConstruct
   @SuppressWarnings("squid:S3011")
   public void initPages() {
     LOG.info("Initializing page objects for class {}", this.getClass());
