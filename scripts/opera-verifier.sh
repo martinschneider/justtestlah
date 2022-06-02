938cf217be03729ef745c1a3841891452e587643
Martin <mart.schneider@gmail.com>
2022-06-02 18:44:41 +0800

    Fix build verifier script for Opera.

--- scripts/opera-verifier.sh
+++ scripts/opera-verifier.sh
@@ -6,4 +6,4 @@ sudo apt-get install opera-stable -y
 sudo apt-get install -y xvfb libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xinput0 libxcb-xfixes0
 sudo /usr/bin/Xvfb "$DISPLAY" -screen 0 1280x1024x24 &
 mvn install -DskipTests && cd justtestlah-core
-mvn test -Djava.version=17 -Dselenide.browserBinary=$(which opera) -Dselenide.headless=false -Dtest=qa.justtestlah.integration.RunnerTest
+mvn test -Djava.version=17 -Dselenide.browserBinary=$(which opera) -Dselenide.browser=opera -Dselenide.headless=false -Dtest=qa.justtestlah.integration.RunnerTest
